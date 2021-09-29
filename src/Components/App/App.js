6ac3c99b4a47773db179f936e8748aae6b439b2d
Tran Ho <tranho8596@gmail.com>
2021-09-28 21:41:18 -0700

    added files

---
+++ src/Components/App/App.js
@@ -0,0 +1,23 @@
+
+import './App.css';
+import React from 'react';
+import Search from '../Search/Search';
+import { render } from 'react-dom';
+
+class App extends React.Component {
+  render(){
+    return (
+      <div className = "App"> 
+        hello
+        
+    
+       </div>
+    
+      
+    );
+    <Search/>
+  }
+  
+}
+
+export default App;
---
+++ src/Components/Search/Search.js
@@ -0,0 +1,10 @@
+import React from "react";
+import './Search.css';
+class Search extends React.Component{
+    render(){
+        return (
+            <div class Name= "Searchbar">This is search bar</div>
+        )
+    };
+}
+export default Search;
