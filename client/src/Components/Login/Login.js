5a5d40a34094c926f07cc21f8a8baaa1b5ee2201
duantruong <duan.truong@sjsu.edu>
2021-12-05 23:26:39 -0800

    search Bar

--- client/src/Components/Login/Login.js
+++ client/src/Components/Login/Login.js
@@ -1,26 +1,51 @@
-import React, { Component } from 'react'
+import React, { Component, useState } from 'react'
+import Axios from 'axios'
 import "./Login.css";
-export default class Login extends Component {
-    render() {
-        return (
-            <div className="d-flex col text-center justify-content-center">
-                <form className="form-signin ">
-                    <h1 className="h3 mb-3 mt-3 font-weight-normal"> Please sign in</h1>
-                    <label for="inputEmail" className="sr-only">
-                        Email Address
-                    </label>
-                    <input type="email" id="inputEmail" className="form-control" placeholder="Email Address" required autoFocus>
-                    </input>
-                    <label for="inputPassword" className="sr-only">
-                        Password
-                    </label>
-                    <input type="email" id="inputPassword" className="form-control" placeholder="Password" required></input>
-                    <button className="btn btn-lg btn-primary btn-block m-3 " type="submit"> Sign in </button>
-                    <button className="btn btn-lg btn-primary btn-block m-3 " type="submit"> Sign up </button>
+export default function Login() {
+    const[email,setEmail] = useState("")
+    const[Password,setPassword] = useState("")
 
-                    
-                </form>
-            </div>
-        )
+    const[loginStatus, setLoginStatus] = useState("")
+    const login = () => {
+        Axios.post('http://localhost:8081/api/user/login',{
+            email:email,Password:Password
+        }).then(
+            (response) =>{
+
+
+                if(response.data.message){
+                    setLoginStatus(response.data.message)
+                }else{
+                    setLoginStatus(response.data[0].email)
+                }
+                //console.log(response.data);
+            });
     }
+    return (
+        <div className="d-flex col text-center justify-content-center">
+            <form className="form-signin ">
+                <h1 className="h3 mb-3 mt-3 font-weight-normal"> Please sign in</h1>
+                <label htmlFor="inputEmail" className="sr-only">
+                    Email Address
+                </label>
+                <input
+                    onChange={(e)=>setEmail(e.target.value)}
+                    type="email" id="inputEmail" className="form-control" placeholder="Email Address" required autoFocus>
+                </input>
+                <label htmlFor="inputPassword" className="sr-only">
+                    Password
+                </label>
+                <input
+                    onChange={(e)=>setPassword(e.target.value)}
+                    type="password" id="inputPassword" className="form-control" placeholder="Password" required>
+
+                </input>
+                <button onClick={login} className="btn btn-lg btn-primary btn-block m-3 " type="submit"> Log in </button>
+
+                <h1> {loginStatus}</h1>
+
+            </form>
+        </div>
+    )
+
 }
