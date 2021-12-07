47a80c366752fe79e94f5f9e10954e094cf272e4
duantruong <duan.truong@sjsu.edu>
2021-12-06 16:15:20 -0800

    Updated Css for search bar

--- client/src/Components/DataServices.js
+++ client/src/Components/DataServices.js
@@ -3,10 +3,10 @@ import React from "react";
 
 export default class DataServices{
     static retrieveAllProduct() {
-        return axios.get('http://localhost:8081/all');
+        return axios.get('http://localhost:8081/api/product/all');
     }
     static searchByType(category){
-        return axios.get(`http://localhost:8081/searchByType/${category}`);
+        return axios.get(`http://localhost:8081/api/product/searchByType/${category}`);
     }
     static addToCart(){
         return axios.post('http://localhost:8081/api/order/addToCart',{
